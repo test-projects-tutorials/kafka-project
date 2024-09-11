21d1f63701d2db41817bd4a255ce312b9dbb1210
Vadym S <vadyms@yodagu.com>
2024-09-12 02:52:24 +0300

    initialize

---
+++ src/main/java/pet/kafkaexamples/KafkaExamplesApplication.java
@@ -0,0 +1,13 @@
+package pet.kafkaexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExamplesApplication.class, args);
+    }
+
+}
